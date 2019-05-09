13efd21228281bd92aee3e1763653c6e41d6aada
Joseph Luna <josephluna@Josephs-MBP.local>
2019-05-08 20:24:39 -0500

    committing social card MVP complete lacking stretch moving onto Calculator

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+    <div className="header-content-wrapper">
+     <h1>Lambda School</h1> 
+    <span><a href="#">@LambdaSchool</a> &middot; 7 March</span>
+    </div>
+    )
+}
+
+export default HeaderTitle
